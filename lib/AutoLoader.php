b47a403cd5cbd2968602de7177dabaf8cf8580c5
Feher Vilmos <jupposessho@gmail.com>
2014-02-17 00:22:32 +0100

    readme

--- lib/AutoLoader.php
+++ lib/AutoLoader.php
@@ -2,7 +2,7 @@
 class AutoLoader {
 	static public function loader($className)
 	{
-		$filename = __DIR__."/vendor/" . str_replace('\\', '/', $className) . ".php";
+		$filename = __DIR__."/src/" . str_replace('\\', '/', $className) . ".php";
 		if (file_exists($filename)) {
 			include($filename);
 			if (class_exists($className)) {
